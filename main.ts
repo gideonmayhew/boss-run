b7c971c18b0fe1888835e4a56e4a708680479c9a
gideonmayhew <64711719+gideonmayhew@users.noreply.github.com>
2020-09-18 13:39:05 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,50 +2,72 @@ namespace SpriteKind {
     export const boss = SpriteKind.create()
     export const bossprjetil = SpriteKind.create()
 }
-namespace myTiles {
-    //% blockIdentity=images._tile
-    export const tile0 = img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`
-    //% blockIdentity=images._tile
-    export const tile1 = img`
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-`
-}
 sprites.onOverlap(SpriteKind.bossprjetil, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
     sprite.destroy(effects.disintegrate, 10)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = jump
+    }
+})
+function doSomething () {
+    bosshellth = 100
+    time_betwen_boss_fire = 100
+    time_bettwen_boss_move = 2000
+    the_boss = sprites.create(img`
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        cccccccccccccccccccccccc
+        `, SpriteKind.boss)
+    the_boss.setPosition(145, 54)
+    the_boss.setVelocity(0, 50)
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.boss, function (sprite, otherSprite) {
     sprite.destroy(effects.disintegrate, 10)
     bosshellth += -1
@@ -55,111 +77,45 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.boss, function (sprite, othe
         time_bettwen_boss_move = 1000
     }
 })
-function doSomething () {
-    bosshellth = 100
-    time_betwen_boss_fire = 100
-    time_bettwen_boss_move = 2000
-    the_boss = sprites.create(img`
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c 
-`, SpriteKind.boss)
-    the_boss.setPosition(145, 54)
-    the_boss.setVelocity(0, 50)
-}
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = jump
-    }
-})
 let bime_sinse_lest_boss = 0
 let is_boss_ataking = false
 let projectile: Sprite = null
 let lastfiretime = 0
 let the_boss: Sprite = null
-let time_betwen_boss_fire = 0
 let time_bettwen_boss_move = 0
+let time_betwen_boss_fire = 0
 let bosshellth = 0
 let jump = 0
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, SpriteKind.Player)
-tiles.setTilemap(tiles.createTilemap(
-            hex`0a0008000000000000000000000000000001010100000000000000000000000000000101000000000000000000000000000000000000000001010100000000000000000000000000000001010101010101010101`,
-            img`
-. . . . . . . . . . 
-. . . 2 2 2 . . . . 
-. . . . . . . . . . 
-2 2 . . . . . . . . 
-. . . . . . . . . . 
-. . 2 2 2 . . . . . 
-. . . . . . . . . . 
-2 2 2 2 2 2 2 2 2 2 
-`,
-            [myTiles.tile0,myTiles.tile1],
-            TileScale.Sixteen
-        ))
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    `, SpriteKind.Player)
+tiles.setTilemap(tiles.createTilemap(hex`3c000800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010100000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000002000000000000000000000000000000000000000101000000000000020000000000020000000000000100000000000000000000000000000000000000000000000001010000020000000200000000000000000000000000000000000000000000000000010000000000000200000000000000010100000000000101000000000000000000000000010101000000000000010100000000010100000000000000000000000000000000000000000000000000000000000000000000000101010000000000000000000000000000000000000000000000000000000001010100000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+    ............................................................
+    .................222........................................
+    ............................22...................2..........
+    ..............22................................2...........
+    ...22.....22............222......22....22...................
+    ................222............................222..........
+    .......2....................................................
+    222222222222222222222222222222222222222222222222222222222222
+    `, [myTiles.tile0,myTiles.tile1,myTiles.tile2], TileScale.Sixteen))
+scene.cameraFollowSprite(mySprite)
 info.setLife(10)
 let gravity = 300
 jump = 32
@@ -171,9 +127,6 @@ mySprite.ay = gravity
 controller.moveSprite(mySprite, r, 0)
 let pleft = false
 doSomething()
-game.onUpdateInterval(100, function () {
-	
-})
 game.onUpdate(function () {
     if (mySprite.vx < 0) {
         pleft = true
@@ -184,18 +137,18 @@ game.onUpdate(function () {
         if (game.runtime() > lastfiretime + timebuttwen) {
             if (pleft) {
                 projectile = sprites.createProjectileFromSprite(img`
-1 1 1 1 
-1 1 1 1 
-1 1 1 1 
-1 1 1 1 
-`, mySprite, 0 - r, 0)
+                    1 1 1 1 
+                    1 1 1 1 
+                    1 1 1 1 
+                    1 1 1 1 
+                    `, mySprite, 0 - r, 0)
             } else {
                 projectile = sprites.createProjectileFromSprite(img`
-1 1 1 1 
-1 1 1 1 
-1 1 1 1 
-1 1 1 1 
-`, mySprite, r, 0)
+                    1 1 1 1 
+                    1 1 1 1 
+                    1 1 1 1 
+                    1 1 1 1 
+                    `, mySprite, r, 0)
             }
             projectile.vx += mySprite.vx
             lastfiretime = game.runtime()
@@ -206,29 +159,39 @@ game.onUpdate(function () {
     if (is_boss_ataking) {
         if (game.runtime() > bime_sinse_lest_boss + time_betwen_boss_fire) {
             projectile = sprites.createProjectileFromSprite(img`
-b b b b 
-b b b b 
-b b b b 
-b b b b 
-`, the_boss, -75, 0)
+                b b b b 
+                b b b b 
+                b b b b 
+                b b b b 
+                `, the_boss, 75, 0)
+            the_boss.setFlag(SpriteFlag.AutoDestroy, false)
+            projectile.setFlag(SpriteFlag.AutoDestroy, false)
             projectile.setKind(SpriteKind.bossprjetil)
-            projectile.y += Math.randomRange(-16, 16)
+            projectile.y += randint(-10, 10)
             bime_sinse_lest_boss = game.runtime()
-            if (bosshellth < 40) {
+            if (bosshellth < 150) {
                 if (the_boss.isHittingTile(CollisionDirection.Bottom) || the_boss.isHittingTile(CollisionDirection.Top)) {
                     projectile = sprites.createProjectileFromSprite(img`
-b b b b 
-b b b b 
-b b b b 
-b b b b 
-`, the_boss, -75, 0)
+                        b b b b 
+                        b b b b 
+                        b b b b 
+                        b b b b 
+                        `, the_boss, -75, 0)
+                    projectile.ax += 50
+                    projectile.ax += -50
+                    the_boss.setFlag(SpriteFlag.AutoDestroy, false)
+                    projectile.setFlag(SpriteFlag.AutoDestroy, false)
                     projectile.setKind(SpriteKind.bossprjetil)
-                    projectile.y += Math.randomRange(-50, 50)
+                    projectile.y += randint(-50, 50)
                     bime_sinse_lest_boss = game.runtime()
                 }
             }
         }
     }
+})
+forever(function () {
+    pause(5000)
+    tiles.placeOnRandomTile(the_boss, myTiles.tile2)
 })
 forever(function () {
     if (the_boss.isHittingTile(CollisionDirection.Bottom) || the_boss.isHittingTile(CollisionDirection.Top)) {
@@ -257,4 +220,7 @@ forever(function () {
         }
     }
     pause(100)
+})
+game.onUpdateInterval(100, function () {
+	
 })
